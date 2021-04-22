b52d621b69dd7be51f51df1fac162a2ea9ce99be
thundermage117 <72457864+thundermage117@users.noreply.github.com>
2021-04-22 19:08:47 +0530

    Added adjacency list

--- header.h
+++ header.h
@@ -20,6 +20,9 @@ struct __person {
 struct __station {
 	_status worst_affected;
 	_list list;
+    int station_id;
+    int weight;
+    struct __station* next;
 };
 struct __list {
 	int person_id;
@@ -34,11 +37,20 @@ struct __route {
 	int station_id;
 	struct __route* next_station;
 };
+struct Network {
+  int total_stations;
+  struct __station** adjLists;
+};
 typedef struct __day _day;
 typedef struct __person _person;
 typedef struct __station _station;
 typedef struct __path _path;
 typedef struct __route _route;
 typedef struct __list _list;
+
+struct __station* create_station(int);                  //Creates a stn, with station_id as input 
+struct Network* createANetwork(int N);                  //Creates a network of N stations
+void addEdge(struct Network*, int s, int d , int w );   //Adds an edge of length w between s and d
+void printNetwork(struct Network*);                     //Prints the Graph
 
 #endif
